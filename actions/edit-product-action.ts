b61db20d9f3ccac78da383dacfd6ff1c8e348f8a
Manu Mdz <manuel.mendozac.dev@gmail.com>
2025-08-28 11:14:16 -0600

    Nextjs - Administracion - CRUD de Productos

---
+++ actions/edit-product-action.ts
@@ -0,0 +1,47 @@
+'use server';
+
+import { ErrorResponseSchema, Product, ProductFormSchema } from '@/src/schemas';
+
+type ActionStateType = {
+    errors: string[];
+    success: string;
+}
+
+export async function updateProduct(productId: Product['id'], prevState: ActionStateType, formData: FormData) {
+    const products = ProductFormSchema.safeParse({
+        name: formData.get('name'),
+        price: formData.get('price'),
+        inventory: formData.get('inventory'),
+        categoryId: formData.get('categoryId'),
+    });
+
+    if (!products.success) {
+        return {
+            errors: products.error.issues.map(issue => issue.message),
+            success: '',
+        }
+    }
+
+    const url = `${process.env.API_URL}/products/${productId}`;
+    const req = await fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(products.data),
+    });
+    const json = await req.json();
+
+    if (!req.ok) {
+        const errors = ErrorResponseSchema.parse(json);
+        return {
+            errors: errors.message.map(issue => issue),
+            success: '',
+        }
+    }
+
+    return {
+        errors: [],
+        success: 'Producto Actualizado Correctamente'
+    }
+}
